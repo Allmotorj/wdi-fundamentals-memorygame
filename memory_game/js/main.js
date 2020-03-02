66648dce7a1f8d31ca7dfae36fa3d8a8c5a92dfa
Jay Reginaldo <jay@jays-mbp.attlocal.net>
2020-03-01 21:28:53 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,31 @@
-console.log("User flipped king");
-console.log("User flipped queen");
-
 let cards = ['queen', 'queen', 'king', 'king'];
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-let cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
-
-} 
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
+function checkForMatch() 
+{
+if (cardsInPlay[0] === cardsInPlay[1]) 
+{
+  console.log ("You found a match!");
+} else {
+	console.log ("Sorry, try again.");
 }
-else {
-	alert("Sorry, try again.")
 }
+
+function flipCard(cardId) {
+ console.log("User Flipped " + cards[cardId]);
+}
+{
+	checkForMatch()
+}
+
+
+// cardsInPlay.push(cards[cardId]);
+
+flipCard()
+
+// if (cardsInPlay.length === 2) {} 
+
+
+
+
+
